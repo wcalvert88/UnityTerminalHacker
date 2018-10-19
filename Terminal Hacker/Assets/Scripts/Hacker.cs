2202c56a965a99a85a4c5d1d6c24c05eceb4d0da
wcalvert88 <wcalvert88@gmail.com>
2018-10-19 16:34:54 -0400

    added conditionals to make terminal respond correctly to user input

--- Terminal Hacker/Assets/Scripts/Hacker.cs
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -7,13 +7,13 @@ public class Hacker : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		
-		ShowMainMenu("Wade");
+		ShowMainMenu();
 		
 	}
 
-	void ShowMainMenu(string greeting) {
+	void ShowMainMenu() {
 		Terminal.ClearScreen();
-		greeting = "Hello " + greeting;
+		string greeting = "Hello Wade"
 		Terminal.WriteLine(greeting);
 		Terminal.WriteLine("What would you like to hack into?\n");
 		Terminal.WriteLine("Press 1 for the school.");
@@ -24,6 +24,18 @@ public class Hacker : MonoBehaviour {
 
 	void OnUserInput(string input)
 	{
-		print(input);
+		if (input == "1") {
+			Terminal.WriteLine("You chose level 1");
+		} else if (input == "2") {
+			Terminal.WriteLine("You chose level 2"); 
+		} else if (input == "3") {
+			Terminal.WriteLine("You chose level 3");
+		} else if (input == "menu") {
+			ShowMainMenu();
+		} else if (input == "007") {
+			Terminal.WriteLine("Please choose a valid level Mr. Bond");
+		} else {
+			Terminal.WriteLine("Please choose a valid level");
+		}
 	}
 }
