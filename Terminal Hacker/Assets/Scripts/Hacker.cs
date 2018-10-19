88fc74206a6b0df740b97c88f1fb2bdf0b61b45e
wcalvert88 <wcalvert88@gmail.com>
2018-10-19 09:19:46 -0400

    Created instructions on the terminal screen

---
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Terminal.WriteLine("What would you like to hack into?\n");
+		Terminal.WriteLine("Press 1 for the school.");
+		Terminal.WriteLine("Press 2 for Google.");
+		Terminal.WriteLine("Press 3 for SpaceX.\n");
+		Terminal.WriteLine("Enter your selection:");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
